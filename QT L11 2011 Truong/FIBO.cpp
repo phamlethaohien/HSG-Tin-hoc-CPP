272d3624e4c94105ae63c83fa1d575149028339c
unknown <phamlethaohien@github.com>
2022-08-08 09:28:20 +0700

    Day Fibonacci

---
+++ QT L11 2011 Truong/FIBO.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int fibonacci (int n) {
+    int f0 = 0;
+    int f1 = 1;
+    int fn = 1;
+    int i;
+
+    if (n < 0) {
+        return -1;
+    } else if ( n == 0 || n == 1) {
+        return n;
+    } else {
+        for (i = 2; i < n; i++) {
+            f0 = f1;
+            f1 = fn;
+            fn = f0 + f1;
+        }
+    }
+    return fn;
+}
+
+int main() {
+    freopen("FIBO.inp", "r", stdin);
+    freopen("FIBO.out", "w", stdout);
+    int n;
+    cin >> n;
+
+    for (int i = 1; i < n+1; i++)
+        cout << fibonacci(i) << " ";
+}
